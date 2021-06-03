71d46c0b824c5dac823e07e973de9c5a14b24f63
Amira-GH <amiraghraiy@gmail.com>
2021-06-03 19:23:20 +0300

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,7 @@
+1- select * from students;
+2-  select * from students where age > 30;
+3- select * from students where age =30 and Gender = 'F';
+4- select Points from students where name='Alex';
+5- insert into students (Name,Age,Gender,Points) VALUES ('Amira',18,'F',500);
+6- update students set Points = 450 where name='Basma';
+7- update students set Points = 100 where name='Alex';
